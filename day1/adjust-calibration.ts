0120b95c8a7d38925c8fdd77032e98a9731b2f43
OrkWard <orkward_o@outlook.com>
2024-02-03 02:55:27 +0800

    feat(day1): day1 part two, use lookahead assersions

--- day1/adjust-calibration.ts
+++ day1/adjust-calibration.ts
@@ -1,7 +1,23 @@
 import * as fs from "node:fs/promises";
 
+const DIGIT_MAP = new Map<string, number>([
+  ["one", 1],
+  ["two", 2],
+  ["three", 3],
+  ["four", 4],
+  ["five", 5],
+  ["six", 6],
+  ["seven", 7],
+  ["eight", 8],
+  ["nine", 9],
+]);
+
 function findFirstLastDigit(str: string): [number, number] {
-  const digits = str.match(/\d/g)?.map((v) => parseInt(v));
+  const digits = Array.from(
+    str.matchAll(/(?=(\d|one|two|three|four|five|six|seven|eight|nine))/g)
+  )
+    .map((v) => v[1])
+    .map((v) => (v.length > 1 ? DIGIT_MAP.get(v)! : parseInt(v)));
   if (!digits || digits.length < 1) {
     throw new Error(`This line don\'t contain two digits: ${str}`);
   }
@@ -9,7 +25,7 @@ function findFirstLastDigit(str: string): [number, number] {
   if (digits.some(Number.isNaN)) {
     throw new Error(`This line contains NaN: ${str}`);
   }
-  return [digits[0], digits[digits?.length - 1]];
+  return [digits[0], digits[digits.length - 1]];
 }
 
 try {
